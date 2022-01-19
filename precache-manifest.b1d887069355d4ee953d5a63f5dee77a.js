38c2aa778d6ac2572d037678e5e8dc0af5718a7d
Sapna_12 <sapnamee09@gmail.com>
2022-01-20 03:50:49 +0530

    Updates

---
+++ precache-manifest.b1d887069355d4ee953d5a63f5dee77a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "81bce7cd0931e585b5e1",
+    "url": "/Music_website/static/css/main.b0e81995.chunk.css"
+  },
+  {
+    "revision": "81bce7cd0931e585b5e1",
+    "url": "/Music_website/static/js/main.8f8cb6d5.chunk.js"
+  },
+  {
+    "revision": "0c7b742a14d1573a134b",
+    "url": "/Music_website/static/js/runtime~main.abef2606.js"
+  },
+  {
+    "revision": "c0c73887a0415c6b3e2b",
+    "url": "/Music_website/static/css/2.f0c41688.chunk.css"
+  },
+  {
+    "revision": "c0c73887a0415c6b3e2b",
+    "url": "/Music_website/static/js/2.c480e2fc.chunk.js"
+  },
+  {
+    "revision": "9ebe477f6423b910385caa5e46d5a88a",
+    "url": "/Music_website/static/media/hero.9ebe477f.jpg"
+  },
+  {
+    "revision": "993128d56f43ca35800b26f7b31aadb2",
+    "url": "/Music_website/index.html"
+  }
+];
